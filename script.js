ad29c02eb76c3ed26438e2d5d8958e102f1be16d
Manasa2710 <manasakaribandi@gmail.com>
2024-09-11 20:46:43 +0530

    script.js

--- script.js
+++ script.js
@@ -31,11 +31,74 @@ function addNewEDField()
 function generateResume()
 {
     // console.log("generating resume");
+    
+    // taking the value from the form field name and storing in variable
     let nameField=document.getElementById("nameField").value;
     
+    // selecting template's name field and storing that field in variable
     let nameT1=document.getElementById("nameT1");
+    
+    // change inner value 
     nameT1.innerHTML=nameField;
     
+    // for contact field
     let nameT2=document.getElementById("nameT2");
     nameT2.innerHTML=nameField;
+
+    let contactField=document.getElementById("contactField").value;
+
+    let contactT=document.getElementById("contactT");
+    contactT.innerHTML=contactField;
+
+    // for address
+    let addressField=document.getElementById("addressField").value;
+    let addressT=document.getElementById("addressT");
+    addressT.innerHTML=addressField;
+
+    // for linkedin and mail
+    let linkedField=document.getElementById("linkedField").value;
+    let linkedT=document.getElementById("linkedT");
+    linkedT.innerHTML=linkedField;
+
+    let emailField=document.getElementById("emailField").value;
+    let emailT=document.getElementById("emailT");
+    emailT.innerHTML=emailField;
+
+    // for objective
+    // let objectiveField=document.getElementById("objectiveField").value;
+    // let objectiveT=document.getElementById("objectiveT");
+    // objectiveT.innerHTML=objectiveField;
+
+    document.getElementById("objectiveT").innerHTML=document.getElementById("objectiveField").value;
+
+    // for work experience
+    let weField=document.getElementsByClassName("weField");
+
+    str="";
+    for (let w of weField) {
+        str = str + `<li> ${w.value} </li>`
+        
+    }
+    
+    document.getElementById("weT").innerHTML=str;
+
+    // for educational qualificaions
+
+    let edField=document.getElementsByClassName("edField");
+    str="";
+    for (let e of edField) {
+        str = str + `<li> ${e.value} </li>`
+        
+    }
+    document.getElementById("edT").innerHTML=str;
+
+    document.getElementById("resume-form").style.display='none';
+    document.getElementById("resume-template").style.display='block';
+
+
+}
+
+
+function printResume(){
+    window.print();
 }
